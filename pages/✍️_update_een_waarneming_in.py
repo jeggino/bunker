3802dec45de16466b7f8abcee3e57802c8de97d8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-20 08:53:49 +0200

    Update ✍️_update_een_waarneming_in.py

--- pages/✍️_update_een_waarneming_in.py
+++ pages/✍️_update_een_waarneming_in.py
@@ -1,2 +1,3 @@
 import streamlit as st
 
+st.image('https://th.bing.com/th/id/R.121ef7c8772dc1652724edf98f52a627?rik=xqdOFbdfJIzCVg&riu=http%3a%2f%2fcpfgc.com%2fimages%2fhomer_notyet.png&ehk=D2Ul2LxB%2friBVsTUg51E%2fZemF%2bTEvTe4nlKvCtHg5dY%3d&risl=&pid=ImgRaw&r=0')
