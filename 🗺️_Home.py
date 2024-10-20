68a71d2905840e74cbd4f1eef9de705a6220fe23
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-20 08:21:37 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -76,7 +76,7 @@ try:
                                                else "Inhabited",axis=1) 
     df_bunkers_features["icon_data"] = df_bunkers_features.apply(lambda x: "icons/bunker_empty.png" 
                                                                  if x['Last survey']=='Uninhabited'
-                                                                 (else "icons/bunker_full.png" if x['Last survey']=='Inhabited'
+                                                                 else ("icons/bunker_full.png" if x['Last survey']=='Inhabited'
                                                                  else 'icons/bunker_no_data.png'), 
                                                                  axis=1)
 except: 
