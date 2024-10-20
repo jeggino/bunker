e738511c43574b19a0ee8d6f3d8bdae94ba943f6
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-20 19:02:13 +0200

    Update ✍️_update_een_waarneming_in.py

--- pages/✍️_update_een_waarneming_in.py
+++ pages/✍️_update_een_waarneming_in.py
@@ -120,10 +120,6 @@ for i in range(len(df_bunkers_features)):
 output = st_folium(map,width=OUTPUT_width, height=OUTPUT_height,returned_objects=["last_object_clicked"],
                    feature_group_to_add=list(functie_dictionary.values()))
 
-st.write(len(output["last_object_clicked"]))
 output["last_object_clicked"]
-try:
-    if len(output["last_object_clicked"]) != 0:
-        input_insert_bats(output,df_bunkers_observations)
-except:
-    st.stop()
+if len(output["last_object_clicked"]) != 0:
+    input_insert_bats(output,df_bunkers_observations)
