6cfc39bd221b6e02eed9426ec5fa0f0a28febf12
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-20 05:46:01 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -3,7 +3,10 @@ OUTPUT_width = 1190
 OUTPUT_height = 450
 ICON_SIZE = (60,35)
 
+# --- DATASET CACHE ---
+ttl = '10m'
+ttl_references = '10m'
 
-#--- SIDEBAR ---
+# --- SIDEBAR ---
 IMAGE = "image/logo.png"
 IMAGE_2 ="image/menu.jpg"
