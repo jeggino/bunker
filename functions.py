6acaefc7e637025943ca3605b68d8f8f5af7f29e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-20 07:40:48 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -4,6 +4,10 @@ import pandas as pd
 import datetime
 from datetime import datetime, timedelta, date
 
+import folium
+from folium.plugins import Draw, Fullscreen, LocateControl, GroupedLayerControl
+
+from credentials import *
 
 def tooltip_html(row,df):
     
