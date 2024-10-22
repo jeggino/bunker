d654a528ea980afce1cbb65692d0295ae205d435
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-22 11:20:33 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -83,7 +83,6 @@ for id in df_bunkers_observations.id_bunker.unique():
     except:
         continue
             
-# df_bunkers_features["id_bunker"] = df_bunkers_features["id_bunker"].astype(str)
 df_bunkers_features["Last survey"] = df_bunkers_features["id_bunker"].map(dict_presences)
 df_bunkers_features["icon_data"] = df_bunkers_features.apply(lambda x: "icons/bunker_empty.png" 
                                                              if x['Last survey']=='Uninhabited'
