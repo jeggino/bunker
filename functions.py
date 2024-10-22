d1d8f4843f44bd65238c0e823508160ba22190cc
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-22 09:50:10 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -62,7 +62,7 @@ def tab_popup(df_bunkers_observations):
             df_table = df_bunkers_observations[df_bunkers_observations.id_bunker==id]
             df_table['date'] = pd.to_datetime(df_table['date'],format="%Y-%m-%d")
             df_table.set_index("date",inplace=True)
-            df_table.drop('waarnemer',axis=1,inplace=True)
+            df_table.drop('id_bunker',axis=1,inplace=True)
             df_table.sort_index(inplace=True)
             df_table.dropna(axis=1, how='all',inplace=True)
             df_table.fillna(0, inplace=True)
