e37484770611d7939619eb82e87b62e8ca075fcb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-22 09:56:00 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -206,11 +206,11 @@ def input_insert_bats(output,df):
 
 @st.dialog(" ")
 def popup_table(id_bunker,output,df_bunkers_features,table_dictionary): 
-    df_popup = df_bunkers_features[df_bunkers_features['id_bunker']==id_bunker]
-    st.write(f'Waarnemer: {df_popup['waarnemer'].values}')
-    st.write(f'Temperature: {df_popup['temperature'].values}')
-    st.write(f'Humidity: {df_popup['humidity'].values}')
-    st.write(f'{df_popup['opmerking'].values}')
+    # df_popup = df_bunkers_features[df_bunkers_features['id_bunker']==id_bunker]
+    # st.write(f'Waarnemer: {df_popup['waarnemer'].values}')
+    # st.write(f'Temperature: {df_popup['temperature'].values}')
+    # st.write(f'Humidity: {df_popup['humidity'].values}')
+    # st.write(f'{df_popup['opmerking'].values}')
     try:
         table_dictionary[id_bunker].iloc[:,3:] = table_dictionary[id_bunker].iloc[:,3:].replace({0:'-'})
         st.dataframe(table_dictionary[id_bunker])
