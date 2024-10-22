81e87bc80c16c504724d1bb4559d2a1a89606fab
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-22 11:27:30 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -80,11 +80,11 @@ for id in df_bunkers_observations.id_bunker.unique():
         elif table_dictionary[id].iloc[-1,4:-1].sum() > 0:
             dict_presences[id] = "Inhabited"
     except:
-        dict_presences[id] = "No Data"
+        continue
 
 dict_presences
 
-df_bunkers_features["Last survey"] = df_bunkers_features["id_bunker"].map(dict_presences)
+df_bunkers_features["Last survey"] = df_bunkers_features["id_bunker"].map(dict_presences).fillna("No Data")
 df_bunkers_features["icon_data"] = df_bunkers_features.apply(lambda x: "icons/bunker_empty.png" 
                                                              if x['Last survey']=='Uninhabited'
                                                              else ("icons/bunker_full.png" if x['Last survey']=='Inhabited'
