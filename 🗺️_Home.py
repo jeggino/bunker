57bc1c4b53ce58b1fc4d9bca655505af098109c2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-23 19:48:58 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -90,7 +90,7 @@ df_bunkers_features["icon_data"] = df_bunkers_features.apply(lambda x: "icons/pr
                                                                    else ("icons/empty.png" if x['Last survey']=='Never inhabited during the survey'
                                                              else 'icons/bunker_no_data.png'), 
                                                              axis=1)
-    
+df_bunkers_features    
 map = folium.Map(tiles=None,position=[df_bunkers_features['lat'].mean(),df_bunkers_features['lng'].mean],)
 LocateControl(auto_start=True,position="topright").add_to(map)
 Fullscreen(position="topright").add_to(map)
