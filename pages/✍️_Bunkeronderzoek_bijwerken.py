946bad8da8e0d0364db49b9dab1558238c20faa5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-23 20:09:52 +0200

    Update ✍️_Bunkeronderzoek_bijwerken.py

--- pages/✍️_Bunkeronderzoek_bijwerken.py
+++ pages/✍️_Bunkeronderzoek_bijwerken.py
@@ -52,67 +52,76 @@ st.markdown("""
 </style>
 """ , unsafe_allow_html=True)
 
-
-st.logo(IMAGE_2,  link=None, icon_image=IMAGE_2)
-
-#--- UI ---
-conn = st.connection("gsheets", type=GSheetsConnection)
-df_bunkers_features = conn.read(ttl=ttl,worksheet="bunkers_features")
-df_bunkers_observations = conn.read(ttl=ttl,worksheet="bunkers_observations")
-df_references = conn.read(ttl=ttl_references,worksheet="df_users")
-
-table_dictionary = tab_popup(df_bunkers_observations)
-
-dict_presences = {}
-for id in df_bunkers_observations.id_bunker.unique():
-    try:
-        if table_dictionary[id].iloc[-1,4:-1].sum() == 0:
-            dict_presences[id] = "Uninhabited"
-        elif table_dictionary[id].iloc[-1,4:-1].sum() > 0:
-            dict_presences[id] = "Inhabited"
-    except:
-        dict_presences[id] = "No Data"
-            
-df_bunkers_features["Last survey"] = df_bunkers_features["id_bunker"].map(dict_presences).fillna("No Data")
-df_bunkers_features["icon_data"] = df_bunkers_features.apply(lambda x: "icons/bunker_empty.png" 
-                                                             if x['Last survey']=='Uninhabited'
-                                                             else ("icons/bunker_full.png" if x['Last survey']=='Inhabited'
-                                                             else 'icons/bunker_no_data.png'), 
-                                                             axis=1)
-
-map = folium.Map(tiles=None,position=[df_bunkers_features['lat'].mean(),df_bunkers_features['lng'].mean],)
-LocateControl(auto_start=True,position="topright").add_to(map)
-Fullscreen(position="topright").add_to(map)
-
-functie_dictionary = {}
-functie_len = df_bunkers_features['Last survey'].unique()
-
-for functie in functie_len:
-    functie_dictionary[functie] = folium.FeatureGroup(name=functie)    
-
-for feature_group in functie_dictionary.keys():
-    map.add_child(functie_dictionary[feature_group])
-
-folium.TileLayer('OpenStreetMap',overlay=False,show=True,name="Streets").add_to(map)
-folium.TileLayer(tiles="Cartodb Positron",overlay=False,show=False,name="Light").add_to(map)
-folium.TileLayer('Cartodb dark_matter',overlay=False,show=False,name="Dark").add_to(map)
-folium.TileLayer(tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',attr='Google_map',overlay=False,show=False,name="Satellite").add_to(map)
-
-folium.LayerControl().add_to(map)    
-
-for i in range(len(df_bunkers_features)):
-
-    fouctie_loop = functie_dictionary[df_bunkers_features.iloc[i]['Last survey']]
-
-    folium.Marker([df_bunkers_features.iloc[i]['lat'], df_bunkers_features.iloc[i]['lng']],
-                  icon=folium.features.CustomIcon(df_bunkers_features.iloc[i]["icon_data"], icon_size=ICON_SIZE)
-                 ).add_to(fouctie_loop)
-
-output = st_folium(map,width=OUTPUT_width, height=OUTPUT_height,returned_objects=["last_object_clicked"],
-                   feature_group_to_add=list(functie_dictionary.values()))
-
 try:
-    if len(output["last_object_clicked"]) != 0:
-        input_insert_bats(output,df_bunkers_observations)
+    waarnemer = st.session_state.login['name']
+    
+    st.logo(IMAGE_2,  link=None, icon_image=IMAGE_2)
+    
+    #--- UI ---
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df_bunkers_features = conn.read(ttl=ttl,worksheet="bunkers_features")
+    df_bunkers_observations = conn.read(ttl=ttl,worksheet="bunkers_observations")
+    df_references = conn.read(ttl=ttl_references,worksheet="df_users")
+    
+    table_dictionary = tab_popup(df_bunkers_observations)
+    
+    dict_presences = {}
+    for id in df_bunkers_observations.id_bunker.unique():
+        try:
+            if (table_dictionary[id].iloc[-1,4:-1].sum() == 0) & (table_dictionary[id].iloc[:-1,4:-1].sum().sum() > 0):
+                dict_presences[id] = "Not inhabited in latest survey"
+            elif table_dictionary[id].iloc[-1,4:-1].sum() > 0:
+                dict_presences[id] = "Inhabited in latest survey"
+            elif len(table_dictionary[id].iloc[:,4:-1].sum()) == 0:
+                dict_presences[id] = "Never inhabited during the survey"
+                
+        except:
+            continue
+    
+    df_bunkers_features["Last survey"] = df_bunkers_features["id_bunker"].map(dict_presences).fillna("No Data")
+    df_bunkers_features["icon_data"] = df_bunkers_features.apply(lambda x: "icons/bunker_previous.png" 
+                                                                 if x['Last survey']=='Not inhabited in latest survey'
+                                                                 else ("icons/bunker_full.png" if x['Last survey']=='Inhabited in latest survey'
+                                                                       else ("icons/bunker_empty.png" if x['Last survey']=='Never inhabited during the survey'
+                                                                 else 'icons/bunker_no_data.png')), 
+                                                                 axis=1)
+    
+    map = folium.Map(tiles=None,position=[df_bunkers_features['lat'].mean(),df_bunkers_features['lng'].mean],)
+    LocateControl(auto_start=True,position="topright").add_to(map)
+    Fullscreen(position="topright").add_to(map)
+    
+    functie_dictionary = {}
+    functie_len = df_bunkers_features['Last survey'].unique()
+    
+    for functie in functie_len:
+        functie_dictionary[functie] = folium.FeatureGroup(name=functie)    
+    
+    for feature_group in functie_dictionary.keys():
+        map.add_child(functie_dictionary[feature_group])
+    
+    folium.TileLayer('OpenStreetMap',overlay=False,show=True,name="Streets").add_to(map)
+    folium.TileLayer(tiles="Cartodb Positron",overlay=False,show=False,name="Light").add_to(map)
+    folium.TileLayer('Cartodb dark_matter',overlay=False,show=False,name="Dark").add_to(map)
+    folium.TileLayer(tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',attr='Google_map',overlay=False,show=False,name="Satellite").add_to(map)
+    
+    folium.LayerControl().add_to(map)    
+    
+    for i in range(len(df_bunkers_features)):
+    
+        fouctie_loop = functie_dictionary[df_bunkers_features.iloc[i]['Last survey']]
+    
+        folium.Marker([df_bunkers_features.iloc[i]['lat'], df_bunkers_features.iloc[i]['lng']],
+                      icon=folium.features.CustomIcon(df_bunkers_features.iloc[i]["icon_data"], icon_size=ICON_SIZE)
+                     ).add_to(fouctie_loop)
+    
+    output = st_folium(map,width=OUTPUT_width, height=OUTPUT_height,returned_objects=["last_object_clicked"],
+                       feature_group_to_add=list(functie_dictionary.values()))
+    
+    try:
+        if len(output["last_object_clicked"]) != 0:
+            input_insert_bats(output,df_bunkers_observations)
+    except:
+        pass
+
 except:
-    pass
+    st.switch_page("🗺️_Home.py")
