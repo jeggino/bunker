d970967edcac66fc2b8938335ea31400bc253b14
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-24 11:40:55 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -128,8 +128,8 @@ def input_data(output,df):
 def input_insert_bats(output,df):
     waarnemer = st.session_state.login['name']
     date = st.date_input("Datum")
-    temperature = st.number_input("Temperature (C°)",format="%i")
-    humidity = st.number_input("Humidity (%)", min_value=1,max_value=100)
+    temperature = st.number_input("Temperature (C°)",values=8)
+    humidity = st.number_input("Humidity (%)", min_value=1,max_value=100,values=40)
     sp = st.multiselect("Chose which species was there", BAT_NAMES)
     
     
