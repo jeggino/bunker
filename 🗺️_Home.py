d6590c5709003536cad472c1a8b173e4c3273628
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-06 10:09:52 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -129,8 +129,10 @@ for i in range(len(df_bunkers_features)):
 
     folium.Marker([df_bunkers_features.iloc[i]['lat'], df_bunkers_features.iloc[i]['lng']],
                   icon=folium.plugins.BeautifyIcon(icon_shape=icon,
+                                                   icon='',
+                                                   border_width=2,
                                    background_color=color,
-                                   border_color=color
+                                   border_color='black'
                                   )
                  ).add_to(fouctie_loop)
 
