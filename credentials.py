dfc81541a2664e5684e144659bf74255a5e516dd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-06 10:26:32 +0100

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -19,6 +19,8 @@ SURROUNDING_OPTIONS = ['In de duinen', 'Loofbos', 'Pijnboombos/groenblijvend bos
 
 BATBOX_SHAPE_OPTIONS = ['Platte kast', 'Ronde kast']
 
+BATBOX_KRAAMVEBLIJFKAST_OPTION = ['Ja','Nee']
+
 TYPE_BUNKER_OPTIONS = ['Open','Niet toegankelijk']
 
 BAT_NAMES = ['Gewone dwergvleermuis','Ruige dwergvleermuis', 'Laatvlieger','Rosse vleermuis','Gewone grootoorvleermuis','Meervleermuis','Watervleermuis','...Andere(n)']
