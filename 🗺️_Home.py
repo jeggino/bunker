ad562ab84b712ad706a160e7b6ec1996008c97f1
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-19 09:13:18 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -102,7 +102,8 @@ for feature_group in functie_dictionary.keys():
 folium.TileLayer('OpenStreetMap',overlay=False,show=True,name="Streets").add_to(map)
 folium.TileLayer(tiles="Cartodb Positron",overlay=False,show=False,name="Light").add_to(map)
 folium.TileLayer('Cartodb dark_matter',overlay=False,show=False,name="Dark").add_to(map)
-folium.TileLayer(tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',attr='Google_map',overlay=False,show=False,name="Satellite").add_to(map)
+folium.TileLayer(tiles='https://api.mapbox.com/styles/v1/jeggino/cm2vtvb2l000w01qz9wet0mv9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamVnZ2lubyIsImEiOiJjbHdscmRkZHAxMTl1MmlyeTJpb3Z2eHdzIn0.N9TRN7xxTikk235dVs1YeQ',
+                 attr='XXX Mapbox Attribution',overlay=False,show=False,name="Satellietkaart").add_to(map)
 
 folium.LayerControl(draggable= True,position ='topright').add_to(map)    
 
