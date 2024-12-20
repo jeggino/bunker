0d2c5c296000e1e0b116216dd99ff4fe1d905d96
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-20 10:00:56 +0100

    Update 2_✍️_Onderzoek_bijwerken.py

--- pages/2_✍️_Onderzoek_bijwerken.py
+++ pages/2_✍️_Onderzoek_bijwerken.py
@@ -55,7 +55,7 @@ st.markdown("""
 try:
     waarnemer = st.session_state.login['name']
     
-    st.logo(IMAGE,  link=None, icon_image=IMAGE)
+    st.logo(IMAGE,  link=None, size="large",icon_image=IMAGE)
     
     #--- UI ---
     conn = st.connection("gsheets", type=GSheetsConnection)
