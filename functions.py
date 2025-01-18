5b32dbb958a634e0cfadbf3bd7063eb4269a6ba3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-18 08:36:06 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -216,21 +216,21 @@ def popup_table(id_bunker,output,df_bunkers_features,table_dictionary):
     st.write(f'{df_popup['opmerking'].loc[0]}')
 
     # try:
-    st.header('Gevonden soorten',divider='grey')
+    # st.header('Gevonden soorten',divider='grey')
 
-    if len(table_dictionary[id_bunker].iloc[:,4:-1].columns) ==0:
-        st.write("Nog geen soort gevonden")
-    else:
-        table_dictionary[id_bunker]
-        for species in table_dictionary[id_bunker].iloc[:,6:].columns:
-            st.write(f'*{species}*')
-            table_dictionary[id_bunker].iloc[:,6:]
-            df = table_dictionary[id_bunker].iloc[:,6:]
-            df 
-            st.write(f"""
-            Het maximale aantal individuen werd bereikt :blue-background[**{int(df[species].max())}**], 
-            gedocumenteerd op datum :blue-background[**{df[df[species]==df[species].max()].index[0]}**].
-            """)
+    # if len(table_dictionary[id_bunker].iloc[:,4:-1].columns) ==0:
+    #     st.write("Nog geen soort gevonden")
+    # else:
+    #     table_dictionary[id_bunker]
+    #     for species in table_dictionary[id_bunker].iloc[:,6:].columns:
+    #         st.write(f'*{species}*')
+    #         table_dictionary[id_bunker].iloc[:,6:]
+    #         df = table_dictionary[id_bunker].iloc[:,6:]
+    #         df 
+    #         st.write(f"""
+    #         Het maximale aantal individuen werd bereikt :blue-background[**{int(df[species].max())}**], 
+    #         gedocumenteerd op datum :blue-background[**{df[df[species]==df[species].max()].index[0]}**].
+    #         """)
         
     st.header('Onderzoeken',divider='grey')
 
