9f8158f4d34752e732fb846a0ab080a8b53771b1
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-18 10:42:04 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -298,13 +298,12 @@ def update_item(id,df):
 
 @st.dialog(" ")
 def delete_item(id):
-    st.write(ciao)
-  # if st.button("Let op! Klik hier als je de waarneming wilt verwijderen",icon="🚨",use_container_width=True):
-  #     response = (
-  #         supabase.table("bunkers_features")
-  #         .delete()
-  #         .eq("id_bunker", id)
-  #         .execute()
-  #         )
-      # st.switch_page("🗺️_Home.py")
+  if st.button("Let op! Klik hier als je de waarneming wilt verwijderen",icon="🚨",use_container_width=True):
+      response = (
+          supabase.table("bunkers_features")
+          .delete()
+          .eq("id_bunker", id)
+          .execute()
+          )
+      st.switch_page("🗺️_Home.py")
     
